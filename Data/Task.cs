869057c8a1f99fbbd9eb0a5d943dbae50a545fde
michalbrzyzek <michalbrzyzek5@gmail.com>
2023-10-19 13:05:07 +0200

    Added validation to edit task name, changed some styles

--- Data/Task.cs
+++ Data/Task.cs
@@ -2,6 +2,7 @@ namespace ToDoList.Data;
 public class Task
 {
     public string Id { get; set; }
+    public int SortId { get; set; }
     public string WhatToDo { get; set; }
     public bool isDone { get; set; }
 }
