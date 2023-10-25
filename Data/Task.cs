3e9e4541d6fa6ee62140b9dd58a321bd7393a01b
michalbrzyzek <michalbrzyzek5@gmail.com>
2023-10-25 17:06:52 +0200

    Added component responsible for displaying items, tasks

--- Data/Task.cs
+++ Data/Task.cs
@@ -2,8 +2,7 @@ namespace ToDoList.Data;
 public class Task
 {
     public string Id { get; set; }
-    public int SortId { get; set; }
+    public DateTime WhenAdded { get; set; }
     public string WhatToDo { get; set; }
     public bool isDone { get; set; }
-    public bool isDisabled { get; set; }
 }
